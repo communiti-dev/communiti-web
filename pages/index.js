73244e9e03c09eb635b29bfe0d992894c0abed5c
Krishna Kant Sharma <31980947+krishna7860@users.noreply.github.com>
2022-08-13 02:35:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,11 +18,19 @@ import Hero from '../components/Hero';
 import Partners from '../components/Partners';
 import { Slider } from '../components/Slider';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
+        <Head>
+          <title>Communiti | Learn & Build</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <Hero />
         <Container maxWidth="lg">
           <Box>
